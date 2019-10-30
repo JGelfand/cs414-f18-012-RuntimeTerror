7369364a77afc073a84c2c8b44aab3157da09b3c
Joseph Gelfand <j.n.gelfand@gmail.com>
2019-10-30 10:58:17 -0600

    Users can now send messages and invites to other users via api (server side only)

--- rollerball/src/main/java/server/api/AuthenticatedRequest.java
+++ rollerball/src/main/java/server/api/AuthenticatedRequest.java
@@ -2,6 +2,9 @@ package server.api;
 
 public class AuthenticatedRequest {
     protected  AuthenticationToken token;
+    public int getAccountId(){
+        return token.getAccountId();
+    }
     public boolean verify(){
         return token.verify();
     }
